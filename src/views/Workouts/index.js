44a2de414fa069d5801d4bb51dcf2d74a0a3575d
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-10-02 15:55:02 +0300

    Removed redundant code from workouts. Update -> Save

--- src/views/Workouts/index.js
+++ src/views/Workouts/index.js
@@ -1,13 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeWorkout } from 'store/slices/user';
 import { userSelector } from 'store';
 import { db } from '../../firebase';
 import uuid from 'react-uuid';
 import { areWorkoutsDifferent } from 'helpers';
 import WorkoutsEl from './Workouts.style';
-import WorkoutForm from 'components/Workouts/WorkoutForm';
-import Modal from 'components/Modal';
 import Spinner from 'components/Spinner';
 import { setDbAppData } from 'store/slices/user';
 import SingleWorkout from 'components/Workouts/SingleWorkout';
@@ -30,9 +27,6 @@ const Workouts = () => {
 	const workoutsRef = useRef();
 
 	const addHandler = () => {
-		// setIsModalOpened(true);
-		// setMode('adding');
-
 		const newWorkout = {
 			id: uuid(),
 			name: '',
@@ -40,63 +34,20 @@ const Workouts = () => {
 		};
 
 		setCurrentWorkout(newWorkout);
-
 		setWorkouts([...workouts, newWorkout]);
 	};
 
-	const deleteWorkout = (e, id) => {
-		e.stopPropagation();
-		dispatch(removeWorkout(id));
-	};
-
-	const editWorkout = (e, id) => {
-		const workout = workouts.find(item => item.id === id);
-		// setMode('editing');
-		// setIsModalOpened(true);
-		setCurrentWorkout(workout);
-	};
-
-	const closeModal = () => {
-		// setIsModalOpened(false);
-		setCurrentWorkout({});
-	};
-
-	// const dispatchWorkouts = () => {
-	// 	const newWorkouts = [...workoutsRef.current.children].map(workout => {
-
-	// 		const name = workout.querySelector('#name').value;
-
-	// 		const exercises = [...workout.querySelectorAll('.exercises-group')].map(
-	// 			group => {
-	// 				return group.querySelector('input').value;
-	// 			}
-	// 		);
-
-	// 		return {
-	// 			name,
-	// 			exercises,
-	// 		};
-	// 	});
-
-	// 	console.log(newWorkouts);
-	// };
-
-	const updateHandler = () => {
-		// dispatchWorkouts();
-		setTimeout(() => {
-			console.log('dbWorkouts: ', dbWorkouts);
-			console.log('workouts: ', workouts);
-			if (!areWorkoutsDifferent(dbWorkouts, workouts)) return;
-			db.collection('users').doc(user.uid).set({
-				workouts,
-			});
-			dispatch(
-				setDbAppData({
-					...appData,
-					workouts,
-				})
-			);
+	const saveWorkouts = () => {
+		if (!areWorkoutsDifferent(dbWorkouts, workouts)) return;
+		db.collection('users').doc(user.uid).set({
+			workouts,
 		});
+		dispatch(
+			setDbAppData({
+				...appData,
+				workouts,
+			})
+		);
 	};
 
 	useEffect(() => {
@@ -123,19 +74,6 @@ const Workouts = () => {
 
 	return (
 		<>
-			{/* {isModalOpened ? (
-				<Modal
-					heading={mode === 'adding' ? 'Add a workout' : 'Edit workout'}
-					closeCb={closeModal}
-				>
-					<div>
-						<WorkoutForm workout={currentWorkout} mode={mode} />
-					</div>
-				</Modal>
-			) : (
-				''
-			)} */}
-
 			<WorkoutsEl className='container workouts'>
 				<h1>Your Workouts</h1>
 				<section className='workouts-list'>
@@ -157,19 +95,6 @@ const Workouts = () => {
 						{workouts.map(workout => {
 							const opened = workout.id === currentWorkout.id;
 							return (
-								// <div
-								// 	className='single-workout'
-								// 	key={workout.id}
-								// 	onClick={e => editWorkout(e, workout.id)}
-								// >
-								// 	{workout.name}
-								// 	<div
-								// 		className='close-icon'
-								// 		onClick={e => deleteWorkout(e, workout.id)}
-								// 	>
-								// 		&times;
-								// 	</div>
-								// </div>
 								<SingleWorkout
 									key={workout.id}
 									workout={workout}
@@ -182,9 +107,9 @@ const Workouts = () => {
 					<footer className='bottom'>
 						<button
 							className='btn btn-green btn-rounded'
-							onClick={updateHandler}
+							onClick={saveWorkouts}
 						>
-							Update Workouts
+							Save
 						</button>
 					</footer>
 				</section>
